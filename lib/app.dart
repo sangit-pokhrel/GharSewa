094de1996404e850ed172c5c3be3ff6951bc5535
sike4356789 <bojib30525@kimasoft.com>
2025-05-17 21:56:28 +0545

    project Creation

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:ghar_sewa/view/splash_screen_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Ghar Sewa Test",
+      initialRoute: '/',
+      routes: {'/': (context) => const SplashScreen()},
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:ghar_sewa/app.dart';
+
+void main() {
+  runApp(
+    MyApp(), // Body
+  );
+}
