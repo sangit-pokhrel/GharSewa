a7d763a99d93471bded0c2d7af53d721abdbd6cb
sangit-pokhrel <heypsycho339@gmail.com>
2025-05-30 15:05:42 +0545

    added some changes on navbar

--- lib/common/bottom_navbar.dart
+++ lib/common/bottom_navbar.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:ghar_sewa/view/home_screen.dart';
+import 'package:ghar_sewa/navbar_screens/city_screen.dart';
+import 'package:ghar_sewa/navbar_screens/search_screen.dart';
+import 'package:ghar_sewa/navbar_screens/order_screen.dart';
+import 'package:ghar_sewa/navbar_screens/profile_screen.dart';
 
 class BottomNavBar extends StatelessWidget {
   final int currentIndex;
   final Function(int)? onTap;
 
   const BottomNavBar({super.key, required this.currentIndex, this.onTap});
+
+  void _navigateToPage(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => const HomeScreen()),
+        );
+        break;
+      case 1:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => const CityScreen()),
+        );
+        break;
+      case 2:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => const SearchScreen()),
+        );
+        break;
+      case 3:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => const OrderScreen()),
+        );
+        break;
+      case 4:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => const ProfileScreen()),
+        );
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +53,56 @@ class BottomNavBar extends StatelessWidget {
       selectedItemColor: const Color(0xFF0052CC),
       unselectedItemColor: Colors.grey,
       type: BottomNavigationBarType.fixed,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.location_city), label: "City"),
-        BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: "Order"),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+      onTap: (index) {
+        if (onTap != null) {
+          onTap!(index); // update index if managed by parent
+        }
+        _navigateToPage(context, index); // navigate on click
+      },
+      items: [
+        const BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.location_city),
+          label: "City",
+        ),
+        BottomNavigationBarItem(
+          icon: _buildSearchButton(currentIndex == 2),
+          label: '',
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.shopping_bag),
+          label: "Order",
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: "Profile",
+        ),
       ],
-      onTap: onTap,
+    );
+  }
+
+  Widget _buildSearchButton(bool isSelected) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      decoration: BoxDecoration(
+        color: const Color(0xFF0052CC),
+        borderRadius: BorderRadius.circular(60),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.search_rounded, size: 25, color: Colors.white),
+          const SizedBox(height: 4),
+          const Text(
+            "Search",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
