d344c11106a74e0597c6d02e1631ee048fb6ced3
sangit-pokhrel <heypsycho339@gmail.com>
2025-06-19 16:30:47 +0545

    register repository added

---
+++ lib/features/register/domain/repository/register_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:ghar_sewa/core/error/failure.dart';
+import 'package:ghar_sewa/features/register/domain/entity/register_entity.dart';
+
+abstract interface class IRegisterRepository {
+    Future<Either<Failure, void>> addRegister(RegisterEntity register);
+}
