b2d8fb8b15b1742b06bacac36dfea20c145e32f6
sangit-pokhrel <heypsycho339@gmail.com>
2025-06-19 16:35:44 +0545

    register local data source code fixes

---
+++ lib/features/register/data/data_source/local_datasource/register_local_datasource.dart
@@ -0,0 +1,23 @@
+import 'package:ghar_sewa/core/network/hive_service.dart';
+import 'package:ghar_sewa/features/register/data/model/register_hive_model.dart';
+import 'package:ghar_sewa/features/register/domain/entity/register_entity.dart';
+
+
+
+class RegisterLocalDataSource implements IRegisterLocalDataSource {
+
+  final HiveService _hiveService;
+  //dependency injection
+  RegisterLocalDataSource({required HiveService hiveService}) : _hiveService = hiveService;
+
+  @override
+  Future<void> addRegister(RegisterEntity register) async {
+    try {
+        final registerHiveModel = RegisterHiveModel.fromEntity(register);   
+        await _hiveService.addRegister(registerHiveModel);
+    } catch (e) {
+        throw LocalDatabaseFailure(message: e.toString());
+    }
+  }
+}
+      
