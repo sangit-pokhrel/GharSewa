cb11810119e6ae0227356e7b15e069d6e15734b5
sangit-pokhrel <heypsycho339@gmail.com>
2025-06-19 16:42:33 +0545

    common use case file from app updated

---
+++ lib/features/register/data/repository/local_repository/register_local_repository.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:ghar_sewa/core/error/failure.dart';
+import 'package:ghar_sewa/features/register/data/data_source/local_datasource/register_local_datasource.dart';
+import 'package:ghar_sewa/features/register/domain/entity/register_entity.dart';
+import 'package:ghar_sewa/features/register/domain/repository/register_repository.dart';
+
+class RegisterLocalRepository implements IRegisterRepository {
+    final IRegisterLocalDataSource _registerLocalDataSource;
+
+    RegisterLocalRepository({required IRegisterLocalDataSource registerLocalDataSource}) : _registerLocalDataSource = registerLocalDataSource;
+
+    @override
+    Future<Either<Failure, void>> addRegister(RegisterEntity register) async {
+        try {
+            await _registerLocalDataSource.addRegister(register);
+            return Right(null);
+        } catch (e) {
+            return Left(LocalDatabaseFailure(message: e.toString()));
+        }
+    }
+}
