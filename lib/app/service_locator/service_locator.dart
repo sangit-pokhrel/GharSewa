82d526d167307a0ffcfa7d295513b1171c4b85c2
sangit-pokhrel <heypsycho339@gmail.com>
2025-06-20 15:58:12 +0545

    dependency injection

--- lib/app/service_locator/service_locator.dart
+++ lib/app/service_locator/service_locator.dart
@@ -1,5 +1,9 @@
 import 'package:get_it/get_it.dart';
 import 'package:ghar_sewa/core/network/hive_service.dart';
+import 'package:ghar_sewa/features/login/data/data_source/local_datasource/login_local_datasource.dart';
+import 'package:ghar_sewa/features/login/data/repository/local_repository/login_local_repository.dart';
+import 'package:ghar_sewa/features/login/domain/use_case/check_login_usecase.dart';
+import 'package:ghar_sewa/features/login/presentation/view_model/login_view_model.dart';
 import 'package:ghar_sewa/features/register/data/data_source/local_datasource/register_local_datasource.dart';
 import 'package:ghar_sewa/features/register/data/repository/local_repository/register_local_repository.dart';
 import 'package:ghar_sewa/features/register/domain/use_case/add_register_usecase.dart';
@@ -10,6 +14,7 @@ final serviceLocator = GetIt.instance;
 Future<void> initDependencies() async {
   await _initHiveService();
   await _initRegisterModule();
+  await _initLoginModule();
 }
 
 Future<void> _initHiveService() async {
@@ -44,12 +49,27 @@ Future<void> _initRegisterModule() async {
       addRegisterUsecase: serviceLocator<AddRegisterUsecase>(),
     ),
   );
+}
 
-  // serviceLocator.registerLazySingleton(
-  //   () => BatchViewModel(
-  //     getAllBatchUsecase: serviceLocator<GetAllBatchUsecase>(),
-  //     createBatchUsecase: serviceLocator<CreateBatchUsecase>(),
-  //     deleteBatchUsecase: serviceLocator<DeleteBatchUsecase>(),
-  //   ),
-  // );
+Future<void> _initLoginModule() async {
+  serviceLocator.registerFactory(
+    () => LoginLocalDataSource(hiveService: serviceLocator<HiveService>()),
+  );
+
+  serviceLocator.registerFactory(
+    () => LoginLocalRepository(
+      loginLocalDataSource: serviceLocator<LoginLocalDataSource>(),
+    ),
+  );
+
+  serviceLocator.registerFactory(
+    () => CheckLoginUsecase(
+      loginRepository: serviceLocator<LoginLocalRepository>(),
+    ),
+  );
+
+  serviceLocator.registerLazySingleton(
+    () =>
+        LoginViewModel(checkLoginUsecase: serviceLocator<CheckLoginUsecase>()),
+  );
 }
