c39764ff9c917b6246b5d7e7314b34020675a06c
sangit-pokhrel <heypsycho339@gmail.com>
2025-06-20 12:09:51 +0545

    login hive model done

--- lib/app/constant/hive/hive_table_constant.dart
+++ lib/app/constant/hive/hive_table_constant.dart
@@ -5,4 +5,8 @@ class HiveTableConstant {
   // Register box
   static const int registerTableId = 0;
   static const String registerBox = 'registerBox';
+
+  // Login box
+  static const int loginTableId = 1;
+  static const String loginBox = 'loginBox';
 }
